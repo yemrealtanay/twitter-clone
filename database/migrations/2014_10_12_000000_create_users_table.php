8abf1a99d8824d8a3be8a6ba20f8a546b8a0b267
Furkan Meraloglu <furkanmeraloglu@gmail.com>
2021-08-15 21:47:09 +0300

    Second commit from collaborator

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -17,6 +17,7 @@ class CreateUsersTable extends Migration
             $table->id();
             $table->string('name');
             $table->string('email')->unique();
+            $table->date('birth_date');
             $table->timestamp('email_verified_at')->nullable();
             $table->string('password');
             $table->rememberToken();
