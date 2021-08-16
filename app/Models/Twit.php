561fc61ff4d5fa58d772156754a8f1072e98392e
Furkan Meraloglu <furkanmeraloglu@gmail.com>
2021-08-16 20:47:25 +0300

    Tag and Twit attachment developed

--- app/Models/Twit.php
+++ app/Models/Twit.php
@@ -36,4 +36,31 @@ class Twit extends Model
     {
         return $this->belongsToMany(Tag::class);
     }
+
+    public function diverge_tags_from_content($content)
+    {
+        $tagString = "";
+        if ($content) {
+            $divergedContent = explode(" ", $content);
+            foreach ($divergedContent as $word) {
+                if ($word[0] === '#') {
+                    $tagString = $tagString . " " . $word;
+                }
+            }
+        }
+        return $tagString;
+    }
+
+    public function set_hashtags($tagString)
+    {
+        if ($tagString) {
+            $tagsToAttach = array_unique(array_map('trim', explode(" ", $tagString)));
+            foreach ($tagsToAttach as $tagName) {
+                $hashtag = Tag::firstOrCreate([
+                    'hashtag' => $tagName,
+                ]);
+                $this->tags()->attach($hashtag->id);
+            }
+        }
+    }
 }
