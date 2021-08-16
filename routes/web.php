705e3f611c22151e7ed1de40d65d582b053529e1
yemrealtanay <y.emrealtanay@gmail.com>
2021-08-16 15:52:54 +0300

    UserController bug fixed

--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,10 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::resource('users', UserController::class);
+
+
+
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth'])->name('dashboard');
